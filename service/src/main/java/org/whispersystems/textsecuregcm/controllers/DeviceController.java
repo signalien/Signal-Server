3e8398c9bdaeb2d121789e867a7f3438edceea74
henry <henry.ph2@gmail.com>
2022-03-21 11:16:34 +0800

    Block downgrading sender key support
    Disallow linking an additional device to an account that has already
    upgraded to having sender key support where the linked device does not
    have sender key support. This should prompt the person attempting to
    link the older application to upgrade in order to complete the linking
    process.

--- service/src/main/java/org/whispersystems/textsecuregcm/controllers/DeviceController.java
+++ service/src/main/java/org/whispersystems/textsecuregcm/controllers/DeviceController.java
@@ -243,6 +243,10 @@ public class DeviceController {
   private boolean isCapabilityDowngrade(Account account, DeviceCapabilities capabilities, String userAgent) {
     boolean isDowngrade = false;
 
+    if (account.isSenderKeySupported() && !capabilities.isSenderKey()) {
+      isDowngrade = true;
+    }
+
     if (account.isGv1MigrationSupported() && !capabilities.isGv1Migration()) {
       isDowngrade = true;
     }
