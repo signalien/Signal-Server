ad7b44505bbc4c4880a9d7c7078ad59957028ed4
henry <henry.ph2@gmail.com>
2022-03-23 12:32:23 +0800

    Actually increment the moved "new user" counter.

--- service/src/main/java/org/whispersystems/textsecuregcm/storage/AccountsManager.java
+++ service/src/main/java/org/whispersystems/textsecuregcm/storage/AccountsManager.java
@@ -217,6 +217,7 @@ public class AccountsManager {
 
       if (freshUser) {
         tags = Tags.of("type", "new");
+        newUserMeter.mark();
       } else {
         tags = Tags.of("type", "reregister");
       }
